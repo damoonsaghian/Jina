0b0d8974b23cb599fc2902559093e22538d41c14
Damoon Saghian <damoonsaghian84@gmail.com>
2023-07-21 17:38:08 +0330

    Delete main.c

--- src/main.c
+++
@@ -1,7 +0,0 @@
-/*
-records containing types will be compiled to glib classes and interfaces
-records where all fields are types will be compiled to interfaces
-
-if possible, compile Jina to C, line by line
-this way, GCC messages remain useful, and making a converter in the future would be easy
-*/
