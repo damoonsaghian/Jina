375cc73c28e6fa373b09e971b5dec750ceb6645f
Damoon Saghian <damoonsaghian84@gmail.com>
2023-07-27 00:08:01 +0330

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 /*
 records containing types will be compiled to structs and traits
 records where all fields are types will be compiled to traits
+
+signed crates:
+https://github.com/rust-lang/crates.io/issues/75
+https://theupdateframework.com/
+https://github.com/rust-lang/rfcs/pull/3403
 */
