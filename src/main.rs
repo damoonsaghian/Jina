9149c96af23fc8ca9c3f8fd2c8eb2c73aed08a8e
Damoon Saghian <damoonsaghian84@gmail.com>
2023-07-28 12:01:52 +0330

    Update and rename main.rs to main.c

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-/*
-records containing types will be compiled to structs and traits
-records where all fields are types will be compiled to traits
-
-signed crates:
-https://github.com/rust-lang/crates.io/issues/75
-https://theupdateframework.com/
-https://github.com/rust-lang/rfcs/pull/3403
-*/
