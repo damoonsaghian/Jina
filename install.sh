be2515b96536890cca20116820ca0c4b18602971
Damoon Saghian <damoonsaghian84@gmail.com>
2023-07-29 23:41:10 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-apt-get install gcc
+apt-get install rustc git gnunet
 cd "$(dirname "$0")"
-mkdir -p ".cache/gcc"
-gcc -o .cache/gcc/jina src/*.c
-cp "$project_dir/.cache/gcc/jina" /usr/local/bin/
+mkdir -p ".cache/cargo"
+rustc --out-dir .cache/cargo -o jina 0.rs
+cp "$project_dir/.cache/cargo/release/jina" /usr/local/bin/
