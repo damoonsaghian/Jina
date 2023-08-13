755814f3f2cfd35fe2f7d7ca4dd0725b02f75519
Damoon Saghian <damoonsaghian84@gmail.com>
2023-08-13 14:21:55 +0330

    Add files via upload

--- src/0.rs
+++ src/0.rs
@@ -1,59 +1,21 @@
 /*
-programming languages that compile to Rust
-	https://lunacookies.github.io/lang/
-	https://github.com/alilleybrinker/langs-in-rust
-	https://github.com/koto-lang/koto
-	https://github.com/PistonDevelopers/dyon
-	https://github.com/jfecher/ante
 https://fasterthanli.me/articles/a-half-hour-to-learn-rust
 https://doc.rust-lang.org/reference/introduction.html
 https://veykril.github.io/tlborm/
 https://github.com/oooutlk/structx
+
+programming languages that compile to Rust
+https://lunacookies.github.io/lang/
+https://github.com/alilleybrinker/langs-in-rust
+https://github.com/koto-lang/koto
+https://github.com/PistonDevelopers/dyon
+https://github.com/jfecher/ante
 
 crates are not signed:
 https://github.com/rust-lang/crates.io/issues/75
 https://theupdateframework.com/
 https://github.com/rust-lang/rfcs/pull/3403
 https://github.com/trustification/rust-rfcs/blob/sigstore-rfc/text/0000-sigstore-integration.md
-
-https://github.com/rust-windowing/glutin
-	https://crates.io/crates/glutin-winit
-	https://crates.io/crates/raw-window-handle
-	https://crates.io/crates/winit-main
-https://github.com/Blatko1/wgpu-text
-https://crates.io/crates/glyph_brush
-https://github.com/grovesNL/glyphon
-https://crates.io/crates/swash
-https://crates.io/crates/allsorts
-https://github.com/ToBinio/wgpu-shapes
-https://github.com/jazzfool/e2
-https://github.com/Lichtso/contrast_renderer/
-https://github.com/audulus/vger-rs
-https://github.com/linebender/vello
-https://github.com/chickenbreeder/kaffee
-https://github.com/ggez/ggez
-https://github.com/google/forma
-https://github.com/BVE-Reborn/rend3/
-https://github.com/nanoqsh/dunge
-https://github.com/kas-gui/kas (widgets are not Send; full of macros)
-https://crates.io/crates/image
-	libgraphicsmagick-q16-3 libimlib2
-	libjxl libavif heif webp librsvg2
-https://github.com/rust-av/rust-av
-	https://crates.io/crates/video-rs
-	https://crates.io/crates/rusty_ffmpeg
-	https://crates.io/crates/ac-ffmpeg
-	https://crates.io/crates/ffmpeg-next
-libavfilter
-mp4/matroska/webm containers
-AV1 Opus
-mp3 and vpx
-h264(openh264), h265(libde265), and aac(fdk-aac)
-av1(aom-libs)
-
-pipewire
-
-libwpewebkit: https://github.com/Igalia/cog
-
-widgets: containers, image/video (view and edit), text (view and edit)
 */
+
+fn main() {}
