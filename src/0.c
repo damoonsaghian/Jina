26ddc0fe173e5f8460b9331ef0088bb881f54a8e
Damoon Saghian <damoonsaghian84@gmail.com>
2023-09-25 16:45:42 +0330

    Add files via upload

--- src/0.c
+++ src/0.c
@@ -1,4 +1,14 @@
 /*
+read Jina files in the project
+check line by line
+enter different modes
+fill the table of identifiers and their types
+check for type consistency of Jina
+use libgirepository to check type consistency of imported parts
+generate C code in ".cache/jina"
+*/
+
+/*
 https://www.cairographics.org/threaded_animation_with_cairo/
 multi'threaded GUI (which GTK is not):
 	container widget asks the children to stop drawing, and after receiving their reply, cleans their area,
