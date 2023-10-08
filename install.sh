9609c8ac02dcf7713f3b632c59467f5f3c4afebd
Damoon Saghian <damoonsaghian84@gmail.com>
2023-10-09 00:07:02 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt-get install gcc libgirepository1.0-dev gnunet
+apt-get install gcc gnunet
 
 project_dir="$(dirname "$0")"
 mkdir -p "$project_dir/.cache/gcc"
 
-gcc -o "$project_dir/.cache/gcc/jina" "$project_dir/src/jina.c"
+gcc -o "$project_dir/.cache/gcc/jina" "$project_dir/jina.c"
 cp "$project_dir/.cache/gcc/jina" /usr/local/bin/
--- jina.c
+++ jina.c
@@ -7,29 +7,52 @@ int main(int argc, char **argv) {
 	
 	if (project_dir == NULL) {
 		printf("interactive Jina is not yet implemented");
-		exit(EXIT_SUCCESS);
+		return EXIT_SUCCESS;
 	}
 	
+	// find all the jina files in project_dir, that have changed since the last compilation
+	char **source_files =
+	
 	/*
-	find changed Jina files in the project directory
-	read them line by line
-	enter different modes
-	fill the table of identifiers and their types
-	check fot type consistency of Jina code
-	use libgirepository to check type consistency of external code
+	fill the table of identifiers (module identifiers and local ones) and their types
+	check for type consistency of Jina code
+	use libgirepository to check type consistency with external code
+	remove all "*.c" files
+	move all "*.o" files to ".cache/jina/objects" directory
+		for each unchanged jina file, move the corresponding object file to ".cache/jina" directory
+		remove ".cache/jina/objects" directory
 	generate C code in ".cache/jina"
-	compile changed C files to object files:
-		system("gcc -c \"$project_dir\"/.cache/jina/{changed,c,files}")
+	compile C files to object files:
+		system("gcc -c \"$project_dir\"/.cache/jina/{all,c,files}")
 	then link object files
 		system("gcc -o program_name *.o")
 	*/
 	
-	exit(EXIT_SUCCESS);
+	/*
+	for a class "ClassName" in a Jina file module "mod.jina" create these two files:
+	, a header file named "mod-class-name.h":
+		#pragma once
+		#include <glib-object.h>
+		G_BEGIN_DECLS
+		
+		// type declaration
+		#define MOD_TYPE_CLASS_NAME mod_class_name_get_type()
+		G_DECLARE_FINAL_TYPE (ModeClassName, mode_class_name, MOD, CLASS_NAME, GObject)
+		
+		// method definitions
+		ModClassName *mod_class_name_new (void);
+		
+		G_END_DECLS
+	, a file named "mod-class-name.c":
+		#include "mod-class-name.h"
+		
+		struct _ModClassName {
+			GObject parent_instance;
+			// other members, including private data
+		};
+		
+		G_DEFINE_TYPE (ModClassName, mode_class_name, G_TYPE_OBJECT)
+	*/
+	
+	return EXIT_SUCCESS;
 }
-
-/*
-multi'threaded GUI (which GTK is not):
-	container widget asks the children to stop drawing, and after receiving their reply, cleans their area,
-	then sends the new areas to them to draw into
-https://www.cairographics.org/threaded_animation_with_cairo/
-*/
