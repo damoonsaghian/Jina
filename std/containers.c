3e7cbd624634b021d0487c0129936ef2bfebe353
Damoon Saghian <damoonsaghian84@gmail.com>
2023-10-27 23:53:51 +0330

    Add files via upload

--- std/containers.c
+++ std/containers.c
@@ -1,12 +1,10 @@
 /*
-arrays are implemented in a way that expanding them, and mutating them at the middle, is efficient
-there is a root array that keeps addresses of segments (except the first segment)
-	this array has a fixed size and is stored in stack
-	the last element (if present) points to an array stored on heap, for furthur expantion
-segments have equal sizes
-the first segment is stored on stack (so small arrays do not need memory allocation)
-the remaining segments are stored on heap
-the destructor method frees the heap portion
+arrays are implemented in a way that expanding them, and mutating them in the middle, is efficient
+storage of an array is devided into equal size segments
+the first segment is stored on the stack (so small arrays do not need memory allocation)
+there is a field that if is not NULL, contains the address of an array (on the heap),
+	that keeps addresses and indexes of the following segments (also on the heap)
+the destructor method frees the heap portion of the array (if there is any)
 
 dictionaries
 https://en.wikipedia.org/wiki/Radix_tree
