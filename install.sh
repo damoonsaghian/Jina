6d247c9c35fe47c5f5f8895bd70bc49a898d932c
Damoon Saghian <damoonsaghian84@gmail.com>
2023-12-25 15:06:21 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
-apt-get install cargo
+apt-get install lua5.3 lua-filesystem gcc liburing-dev
 
 project_dir="$(dirname "$0")"
 
-mkdir -p "$project_dir/.cache/cargo"
-cd "$project_dir/.cache/cargo"
-[ -L Cargo.toml ] || {
-	rm -f Cargo.toml
-	ln -s ../../cargo.toml Cargo.toml
-}
-cargo build --release
-
-cp release/jina /usr/local/bin/
+cp "$project_dir/jina.lua" /usr/local/bin/jina
+chmod +x /usr/local/bin/jina
 
 mkdir -p /usr/local/lib/jina/
 cp -r "$project_dir"/std/* /usr/local/lib/jina/
