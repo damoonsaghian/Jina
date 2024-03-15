a53c62bf69260f62ef737266ff902e7e64b4e796
Damoon Saghian <damoonsaghian84@gmail.com>
2024-03-16 00:04:38 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt-get install gcc libglib2.0-dev libflint-dev
 
 project_dir="$(dirname "$0")"
 
-gcc -Wall -Wextra -Wpedantic -lglib-2.0 "$project_dir/*.c" -o "$project_dir/.cache/gcc/jina"
+gcc -Wall -Wextra -Wpedantic -lglib-2.0 "$project_dir/src/0.c" -o "$project_dir/.cache/gcc/jina"
 cp "$project_dir/.cache/gcc/jina" /usr/local/bin/jina
 
 mkdir -p /usr/local/lib/jina/
