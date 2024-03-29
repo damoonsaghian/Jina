5931709448a52d10a40b9d77d8858750c62412fc
Damoon Saghian <damoonsaghian84@gmail.com>
2024-03-30 00:02:05 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+osp-deb install jina,lua5.4,lua-penlight,gcc
+
 project_dir="$(dirname "$0")"
 
 cp "$project_dir/jina.lua" "$HOME/.local/bin/jina"
@@ -12,5 +14,7 @@ cp "$project_dir/.cache/jina/out/*" "$HOME/.local/packages/jina/"
 cat <<-'__EOF__' > "$HOME/.local/packages/jina/uninstall.sh"
 rm "$HOME/.local/bin/jina"
 rm -r "$HOME/.local/packages/jina"
+osp-deb remove jina
+osp-deb remove jina-std
 exit
 __EOF__
