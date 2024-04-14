7644bd3386595ac09d561fbed608c30ffc09f4bf
Damoon Saghian <damoonsaghian84@gmail.com>
2024-04-14 15:02:06 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+[ -f "$HOME/.local/apps/jina/uninstall.sh" ] && sh "$HOME/.local/apps/jina/uninstall.sh"
+
 ospkg-deb add jina lua5.4,lua-penlight,gcc
 
 project_dir="$(dirname "$0")"
@@ -5,9 +7,9 @@ project_dir="$(dirname "$0")"
 mkdir -p "$HOME/.local/apps/jina/"
 
 jina "$project_dir"
-cp "$project_dir/.cache/jina/out/std/libstd.jin.so" "$HOME/.local/apps/jina/"
+ln "$project_dir/.cache/jina/out/std/libstd.jin.so" "$HOME/.local/apps/jina/"
 
-cp "$project_dir/jina/*.lua" "$HOME/.local/apps/jina/"
+ln "$project_dir/jina/*.lua" "$HOME/.local/apps/jina/"
 
 echo '#!/usr/bin/sh
 cd "$HOME/.local/apps"
@@ -15,9 +17,11 @@ exec lua jina/jina.lua
 ' > "$HOME/.local/bin/jina"
 chmod +x "$HOME/.local/bin/jina"
 
-cat <<-'__EOF__' > "$HOME/.local/apps/jina/uninstall.sh"
-rm "$HOME/.local/bin/jina"
-rm -r "$HOME/.local/apps/jina"
+project_path_hash="$(echo -n "$project_dir" | md5sum | cut -d ' ' -f1)"
+
+cat <<-__EOF__ > "$HOME/.local/apps/jina/uninstall.sh"
+rm ~/.local/bin/jina
+rm -r ~/.local/apps/jina
 ospkg-deb remove jina
-ospkg-deb remove jina-std
+ospkg-deb remove jina-$project_path_hash
 __EOF__
