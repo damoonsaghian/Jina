c7f277221234469664a82069d4179682f5a4eac3
Damoon Saghian <damoonsaghian84@gmail.com>
2024-04-18 09:42:53 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ project_dir="$(dirname "$0")"
 mkdir -p "$HOME/.local/apps/jina/"
 
 jina "$project_dir"
-ln "$project_dir/.cache/jina/out/lib/lib.so" "$HOME/.local/apps/jina/libjina.so"
+ln "$project_dir/.cache/jina/out/std/libstd.jin.so" "$HOME/.local/apps/jina/"
 
 ln "$project_dir/jina/*.lua" "$HOME/.local/apps/jina/"
 
