9903a44d614568e448e5d94ee5162520bf5eccc5
Damoon Saghian <damoonsaghian84@gmail.com>
2024-06-03 23:39:40 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 [ -f "$HOME/.local/apps/jina/uninstall.sh" ] && sh "$HOME/.local/apps/jina/uninstall.sh"
 
-ospkg-deb add jina lua5.3,lua-penlight,gcc,gnunet,git
+upm add jina --deb=lua5.3,lua-penlight,gcc,gnunet,git
 
 project_dir="$(dirname "$0")"
 
@@ -21,6 +21,6 @@ project_path_hash="$(echo -n "$project_dir" | md5sum | cut -d ' ' -f1)"
 cat <<-__EOF__ > "$HOME/.local/apps/jina/uninstall.sh"
 rm ~/.local/bin/jina
 rm -r ~/.local/apps/jina
-ospkg-deb remove jina
-ospkg-deb remove jina-$project_path_hash
+upm remove jina
+upm remove jina-$project_path_hash
 __EOF__
