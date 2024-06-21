ca5a42f30e529c61b500242547cae7c2a165acd9
Damoon Saghian <damoonsaghian84@gmail.com>
2024-06-21 23:00:36 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,8 +12,21 @@ ln "$project_dir/.cache/jina/out/std/libstd.jin.so" "$project_dir/.cache/spm/"
 
 ln "$project_dir"/jina/*.lua "$project_dir/.cache/spm/"
 
-echo '#!/usr/bin/sh
-this_script_real_path="$(readlink $0)"
-exec lua "$(dirname "$this_script_real_path")/jina.lua"
-' > "$project_dir/.cache/spm/0"
+cat <<-'__EOF__' > "$project_dir/.cache/spm/0"
+#!/usr/bin/sh
+this_dir = "$(dirname "$(realpath "$0")")"
+args="$(printf "'%s', " "$@")"
+find "$this_dir" -name '?*.lua' -type f -printf "%P\n" | lua5.3 -e "
+	for lua_file_relative_path in io:lines() do
+		local dir_part = string.gsub(lua_file_relative_path, '/[^/]+$', '')
+		local env = _G
+		for subdir in string.gmatch(dir_part, '[^/]+') do
+			env[subdir] = {}
+			env = env[subdir]
+		end
+		loadfile('$this_dir'..'/'..lua_file_relative_path, 'bt', env)()
+	end
+	main($args)
+"
+__EOF__
 chmod +x "$project_dir/.cache/spm/0"
