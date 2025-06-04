c973eeadf05e30b1c7ad3e29960b085f9f0efafd
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-04 14:19:28 +0330

    Add files via upload

--- SPMbuild.sh
+++ SPMbuild.sh
@@ -6,9 +6,10 @@ spm_xcript jina inst/cmd
 spm_import gmp
 spm_import mpfr
 spm_import arrayfire
-spm_import libav
-spm_import sdl
-spm_import sdl-webkit
+
+spm_import qt
+spm_import qt-media
+spm_import qt-webkit
 
 "$build_dir/exec/jina" "$pkg_dir"
 ln "$pkg_dir/.cache/jina/std/build/$TARGET/libstd.jin.so" "$build_dir/exp/lib"
