87a282b040b1ddad6dd9e5b6d683e1e3bd5b07ee
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-08 17:52:36 +0330

    Add files via upload

--- jina/jina.cc
+++ jina/jina.cc
@@ -121,6 +121,7 @@ void generateCcFile(const string& jinfilePath) {
 	messages will be reapted until they return false
 	a linked list containing tuples of actor's ID and memory address
 	GLOBAL_actors_list
+	use mutexes to hold the list of actors and their message queues
 	actors list is partitioned: file handle 1, file handle 2, socket 1, socket 2, user interface
 	each partition can wake up the eventloop to process the partition's messages
 	
@@ -129,10 +130,6 @@ void generateCcFile(const string& jinfilePath) {
 	, the UI remains responsive, even when the number of non'UI actors is extremely large
 	the main loop runs messages of UI actors, and then polls (non'waiting) more events
 		if there is no more messages for UI actors, wait for events
-	
-	use mutexes to hold the list of actors and their message queues
-	https://www.classes.cs.uchicago.edu/archive/2018/spring/12300-1/lab6.html
-	https://docs.gtk.org/glib/struct.RWLock.html
 	*/
 	
 	// open jinFile from jinFilePath
@@ -154,7 +151,7 @@ generateJinaApiFile() {
 int main(int argc, char** argv) {
 	if (argc == 0) {
 		cout << "interactive Jina is not yet implemented";
-		cou << "usage: jina <project_path> [c3c_options]";
+		cout << "usage: jina <project_path> [c3c_options]";
 		return;
 	};
 	
@@ -164,45 +161,36 @@ int main(int argc, char** argv) {
 	
 	// if build target is "ELF freestanding" (kernel or embedded), link statically, and use --no-evloop
 	
-	/*
-	search for .jin files in the project recursively
-	when one is found, its containing directory, and the sibling directories containing .jin files,
-		are the packages that must be compiled and published
-	*/
+	// if --no-evloop is not used
+	// spm import $gnunet_namespace qt
 	
-	// for each package
-	{
-		// if --no-evloop is not used
-		// spm import $gnunet_namespace qt
-		
-		// go through all files (recursively) and find ".p" files, and for each:
-		// read the file and obtain: <gnunet-namespace> <package-name>
-		// spm import <gnunet-namespace> <package-name>
-		
-		// mkdir -p "$project_dir/.cache/jina/cc"
-		
-		/*
-		find all .jin files (recursively), and if it's newer than the last generated .cc file,
-			regenerate the .cc file
-		number of parallel processes will be equal to the number of CPU cores,
-			or the number of .jin files, either one which is smaller
-		*/
-		string cc_file_path = ;
-		string cc_file_mtime = ;
-		string jin_file_mtime = ;
-		if (! jin_file_mtime || ! cc_file_mtime ||
-			jin_file_mtime > cc_file_mtime ||
-			cc_file_mtime > std::time:: // cc_file is from future!
-		) {}
-		
-		// std.jin is compiled to C++ and imported to all the generated C++ files above
-		
-		/*
-		generate a build file, and then build
-		https://ninja-build.org/
-		args given to the build command:
-			$clang_options
-			"$project_dir/.cache/jina/cc"
-		*/
-	}
+	// go through all files (recursively) and find ".p" files, and for each:
+	// read the file and obtain: <gnunet-namespace> <package-name>
+	// spm import <gnunet-namespace> <package-name>
+	
+	// mkdir -p "$project_dir/.cache/jina/cc"
+	
+	/*
+	find all .jin files (recursively), and if it's newer than the last generated .cc file,
+		regenerate the .cc file
+	number of parallel processes will be equal to the number of CPU cores,
+		or the number of .jin files, either one which is smaller
+	*/
+	string cc_file_path = ;
+	string cc_file_mtime = ;
+	string jin_file_mtime = ;
+	if (! jin_file_mtime || ! cc_file_mtime ||
+		jin_file_mtime > cc_file_mtime ||
+		cc_file_mtime > std::time:: // cc_file is from future!
+	) {}
+	
+	// jina-std is compiled to C++ and imported to all the generated C++ files above
+	
+	/*
+	generate a build file, and then build
+	https://ninja-build.org/
+	args given to the build command:
+		$clang_options
+		"$project_dir/.cache/jina/cc"
+	*/
 }
---
+++ jina/spmbuild.sh
@@ -0,0 +1,5 @@
+spm_import clang
+
+# build jina
+
+spm_xcript jina inst/cmd
